378dfcfe9bfd8f0fbe2211bd44b9a5f27bbb00d1
Nishant Kumar <asknishant.39@gmail.com>
2020-05-31 18:20:24 +0530

    Full working app!!

---
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.sqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GroceryAdapter mAdapter;
+    private SQLiteDatabase mDatabase;
+
+    private EditText mEditTextName;
+    private TextView mTextviewAmount;
+    private int mAmount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GroceryDBHelper dbHelper = new GroceryDBHelper(this);
+        mDatabase = dbHelper.getWritableDatabase();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new GroceryAdapter(this,getAllItems());
+        recyclerView.setAdapter(mAdapter);
+
+        mEditTextName = findViewById(R.id.edittext_name);
+        mTextviewAmount = findViewById(R.id.textView_amount);
+
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+        Button buttonAdd = findViewById(R.id.button_add);
+
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increase();
+            }
+        });
+
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrease();
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+    }
+
+    private void increase() {
+        mAmount++;
+        mTextviewAmount.setText(String.valueOf(mAmount));
+    }
+
+    private void decrease() {
+        if(mAmount>0)
+        mAmount--;
+        mTextviewAmount.setText(String.valueOf(mAmount));
+    }
+    private void addItem() {
+        if(mEditTextName.getText().toString().trim().length() == 0 || mAmount == 0){
+            return;
+        }
+        String name = mEditTextName.getText().toString();
+        ContentValues cv = new ContentValues();
+        cv.put(GroceryContract.GroceryEntry.COLUMN_NAME, name);
+        cv.put(GroceryContract.GroceryEntry.COLUMN_AMOUNT, mAmount);
+
+        mDatabase.insert(GroceryContract.GroceryEntry.TABLE_NAME, null, cv);
+        mAdapter.swapCursor(getAllItems());
+        mEditTextName.getText().clear();
+    }
+
+    private Cursor getAllItems() {
+
+        return mDatabase.query(
+                GroceryContract.GroceryEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                GroceryContract.GroceryEntry.COLUMN_TIMESTAMP + " DESC"
+        );
+    }
+}
